bfc9ddaed9d71be7bc5dca1dff3f714ef61d8b9b
Leogan <leogan1399@gmail.com>
2020-01-06 15:20:10 -0500

    Updates

---
+++ precache-manifest.574114da7ff36b5616bb00ee63361cd4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12aed90e0370552d6392c4f696700f7a",
+    "url": "/Leonel_Garcia/index.html"
+  },
+  {
+    "revision": "55619e77aa8c9d1c2418",
+    "url": "/Leonel_Garcia/static/css/main.cf0f8da9.chunk.css"
+  },
+  {
+    "revision": "f1b2051b0eff93c048b9",
+    "url": "/Leonel_Garcia/static/js/2.61cb3d40.chunk.js"
+  },
+  {
+    "revision": "55619e77aa8c9d1c2418",
+    "url": "/Leonel_Garcia/static/js/main.41fa4b31.chunk.js"
+  },
+  {
+    "revision": "2f6a4819289222891931",
+    "url": "/Leonel_Garcia/static/js/runtime~main.a6b47b53.js"
+  },
+  {
+    "revision": "2b0d937021a8535fbb131f116f28c1d0",
+    "url": "/Leonel_Garcia/static/media/background6.2b0d9370.jpg"
+  },
+  {
+    "revision": "ee185e05f4eb6f2d81e3a55922d82265",
+    "url": "/Leonel_Garcia/static/media/clothing_1.ee185e05.png"
+  },
+  {
+    "revision": "c7b7b11ca7bdd7a7498a7b0f228a4b24",
+    "url": "/Leonel_Garcia/static/media/portfolio_project.c7b7b11c.PNG"
+  },
+  {
+    "revision": "1c41fc9f265523e69872be64e88ac974",
+    "url": "/Leonel_Garcia/static/media/project22.1c41fc9f.PNG"
+  }
+]);
